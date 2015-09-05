601c31d8db40316cdd09f9ab2a80b1378b7a40ff
MyLee <lymaipham@yahoo.com>
2015-09-05 23:26:03 +0200

    web5yr

--- script/showHide.js
+++ script/showHide.js
@@ -1,6 +1,9 @@
 $(document).ready(function()
         {
-$("#indexPhoto").mouseover(function() { $("#navIndex").css('visibility','visible').fadeIn(1000); });
+$("#forcastLink").click(function() { 
+	$("#webForecast").css('visibility','visible').fadeIn(1000); 
+
+});
 
 $("#logo").mouseout(function() { $("#navIndex").css('visibility','hidden').fadeOut(); });
 		})
