ba553287bd7b6be4a07134a01dc6694391aec7f6
MyLee <lymaipham@yahoo.com>
2015-09-05 15:35:30 +0200

    color animate-about

--- script/showHide.js
+++ script/showHide.js
@@ -2,5 +2,5 @@ $(document).ready(function()
         {
 $("#indexPhoto").mouseover(function() { $("#navIndex").css('visibility','visible').fadeIn(1000); });
 
-$("#logo").mouseout(function() { $("#navIndex").css('visibility','hidden').fadeOut(1000); });
+$("#logo").mouseout(function() { $("#navIndex").css('visibility','hidden').fadeOut(); });
 		})
