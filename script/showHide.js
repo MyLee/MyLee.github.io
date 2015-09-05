d046633d6b80399570ed8509af4fa750ed98a16e
MyLee <lymaipham@yahoo.com>
2015-09-05 14:21:10 +0200

    Css fix

---
+++ script/showHide.js
@@ -0,0 +1,6 @@
+$(document).ready(function()
+        {
+$("#indexPhoto").mouseover(function() { $("#navIndex").css('visibility','visible').fadeIn(1000); });
+
+$("#logo").mouseout(function() { $("#navIndex").css('visibility','hidden').fadeOut(1000); });
+		})
