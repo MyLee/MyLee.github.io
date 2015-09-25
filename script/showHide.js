0669aec2183a37d80b4a78d23b6a4761ab619f0d
MyLee <lymaipham@yahoo.com>
2015-09-25 23:30:27 +0200

    yrkersroll

--- script/showHide.js
+++ script/showHide.js
@@ -1,9 +1,6 @@
 $(document).ready(function()
         {
-$("#forcastLink").click(function() { 
-	$("#webForecast").css('visibility','visible').fadeIn(1000); 
-
-});
-
-$("#logo").mouseout(function() { $("#navIndex").css('visibility','hidden').fadeOut(); });
+			function toggleDiv(divId){
+				$("#"+divId).fadeToggle();
+			}
 		})
