26a717ba257f52745122b38b22829ea47f541e33
MyLee <lymaipham@yahoo.com>
2015-12-25 17:15:02 +0100

    edit function

--- todolist/js/todo.js
+++ todolist/js/todo.js
@@ -1,9 +1,70 @@
 //declare variants
 var todo=  document.querySelector('#todolist');
+var dones = document.querySelector('#completed');
 var form = document.querySelector('form');
 var input = document.querySelector('#add');
 var field=document.querySelector('#newitem');
-      
+
+//fetch data
+var getlist = function(savelist){
+      var tasklist = new Array;
+      var item = savelist.tostring();
+      var taskstr=localStorage.getItem(item);
+      if(taskstr !=null){
+            tasklist = JSON.parse(taskstr);
+      }
+      return tasklist;
+}
+
+var tasklist = getlist(todo);
+var donelist= getlist(dones);
+
+var addEvents = function(listItem){
+    var delBtn=listItem.querySelector('.delBtn');
+    var doneBtn=listItem.querySelector('.doneBtn');  
+    var inputTask=listItem.getElementsByTagName('span')[0];
+    inputTask.addEventListener('click', editTask)
+    delBtn.addEventListener('click', deleteTask);        
+    doneBtn.addEventListener('click', toggleDone); 
+}
+
+//local save
+document.addEventListener( 'DOMContentLoaded', retrievestate, false );
+
+function storestate() {
+localStorage.tasklist = todo.innerHTML;
+localStorage.donelist= dones.innerHTML;
+};
+
+var retrievestate= function(localSavedList, target){
+    if(localStorage.localSavedList){
+        target.innerHTML = localStorage.localSavedList;
+        var listArray = todo.children;
+    var listLength=listArray.length;
+    for(var i=0; i <listLength; i++){
+        var listItem= listArray[i];
+        addEvents(listItem);
+        }
+    }
+};
+
+var restoreStage = function(){
+    retrievestate(tasklist, todo);
+    retrievestate(donelist, dones);
+}
+
+// function retrievestate() {
+// if ( localStorage.tasklist ) {
+//     todo.innerHTML = localStorage.tasklist;
+//     var listArray = todo.children;
+//     var listLength=listArray.length;
+//     for(var i=0; i <listLength; i++){
+//         var listItem= listArray[i];
+//         addEvents(listItem);
+//     }
+//     };
+// };
+     
 //option buttons
 var createNewItem = function(taskStr){
     //create listItem
@@ -11,9 +72,7 @@ var createNewItem = function(taskStr){
     //task describe
     var taskDescrible= document.createElement('span'); 
     //edit task input(text)
-    var editForm = document.createElement('form');
     var editInput = document.createElement('input');
-    var editSubmit= document.createElement('button');
     //mark as doneButton (checkbox)
     var doneBtn= document.createElement('i');
     //button delete
@@ -24,15 +83,8 @@ var createNewItem = function(taskStr){
     doneBtn.className="col s1 small material-icons doneBtn";
     doneBtn.innerText='done';
     
-    editForm.className='hide editDiv';
-    editForm.setAttribute('action', '#');
-    editForm.setAttribute('method', 'post');
     editInput.type='text';
-    editInput.className='editInput';
-    editSubmit.className='waves-effect waves-light btn'
-    editSubmit.type='submit';
-    editSubmit.value='Change';
-    editSubmit.innerHTML='Change';
+    editInput.className='hide editInput';
     
     taskDescrible.innerHTML= taskStr;
     
@@ -40,18 +92,13 @@ var createNewItem = function(taskStr){
     delBtn.title="Delete";
     delBtn.innerText="delete";
     
-    //append to list  
-    editForm.appendChild(editInput);
-    editForm.appendChild(editSubmit);     
-        
     listItem.appendChild(doneBtn); 
     listItem.appendChild(taskDescrible);
-    listItem.appendChild(editForm);
+    listItem.appendChild(editInput);
     listItem.appendChild(delBtn);
     
-    //add eventListener to buttons
+    //bind actions to buttons
     taskDescrible.addEventListener('click', editTask);
-    editInput.addEventListener('submit', editTask);
     delBtn.addEventListener('click', deleteTask);        
     doneBtn.addEventListener('click', toggleDone);
            
@@ -77,24 +124,42 @@ form.addEventListener('submit', function(ev){
 var editTask = function(ev){
     console.log("edit task...");
     var listItem = this.parentNode;
-    var editDiv=listItem.querySelector('.editForm');
     var editInput= listItem.querySelector('input');
-    var taskDescrible = listItem.querySelector('span');
-    
-    editInput.value= taskDescrible.innerText;     
-    taskDescrible.classList.toggle('hide');
-    editDiv.classList.toggle('hide');
+    var taskDescribe = listItem.querySelector('span');
+    //hide the task describe
+    editInput.value= taskDescribe.innerText;     
+    taskDescribe.classList.toggle('hide');
+    editInput.classList.toggle('hide');
     editInput.focus();   
-    //detech user click enter
-    editDiv.addEventListener('submit', function(ev){
-            taskDescrible.innerText= editInput.value;
-            taskDescrible.classList.toggle('hide');
-            editDiv.classList.toggle('hide');
-            storestate();
-            ev.preventDefault();
-        });
+    //update task    
+    editInput.onkeypress= function(ev){  
+        if (!ev) ev = window.event;     
+        if(ev.which==13){    
+            editInput.focus();  
+            taskDescribe.innerText= editInput.value;
+            taskDescribe.classList.toggle('hide');
+            editInput.classList.toggle('hide');            
+            storestate();        
+        }
+        
+    };
+    //if user click outside edit box, restore task
+    // editInput.addEventListener("focusout", function(){
+    //     console.log('user lose its focus to edit input');
+    //     if (!ev) ev = window.event;
+    //     taskDescribe.classList.toggle('hide');
+    //     editInput.classList.toggle('hide');
+    // })
 };
 
+// var switchback= function(editInput, taskDescribe, ev){
+//     $(editInput).blur(function(ev){  
+//         if (!ev) ev = window.event;      
+//         console.log('user lose its focus to edit input');
+//         taskDescribe.classList.toggle('hide');
+//         editInput.classList.toggle('hide');
+//     });
+// }
 //delete item
 var deleteTask = function(ev){ 
     var listItem = this.parentNode;
@@ -113,56 +178,14 @@ var toggleDone= function(ev){
     storestate();
 };         
 
+var deleteAllTasks = function(){ 
+    var tasklist=document.getElementById('todolist');
+    while(tasklist.firstChild){
+        tasklist.removeChild(tasklist.firstChild);
+    }     
+    storestate();
+};
 
-//fetch data
-var getlist = function(){
-      var tasklist = new Array;
-      var taskstr=localStorage.getItem('todo');
-      if(taskstr !=null){
-            tasklist = JSON.parse(taskstr);
-      }
-      return tasklist;
-}
-
-var addEvents = function(listItem){
-    // var editBtn=listItem.querySelector('.editBtn');
-    var delBtn=listItem.querySelector('.delBtn');
-    var doneBtn=listItem.querySelector('.doneBtn');  
-    var inputTask=listItem.getElementsByTagName('span')[0];
-    //add EventListener to buttons      
-    // editBtn.addEventListener('click', editTask);
-    inputTask.addEventListener('click', editTask)
-    delBtn.addEventListener('click', deleteTask);        
-    doneBtn.addEventListener('click', toggleDone); 
-}
-
-//local save
- document.addEventListener( 'DOMContentLoaded', retrievestate, false );
-  
-  function storestate() {
-    localStorage.tasklist = todo.innerHTML;
-  };
-
-  function retrievestate() {
-    if ( localStorage.tasklist ) {
-        todo.innerHTML = localStorage.tasklist;
-        var listArray = todo.children;
-        var listLength=listArray.length;
-        for(var i=0; i <listLength; i++){
-            var listItem= listArray[i];
-            addEvents(listItem);
-        }
-       };
-  };
-
- var deleteAllTasks = function(){ 
-        var tasklist=document.getElementById('todolist');
-        while(tasklist.firstChild){
-            tasklist.removeChild(tasklist.firstChild);
-        }     
-        storestate();
-  };
-  
 var clearAllBtn = document.querySelector("#clearAll");
 clearAllBtn.addEventListener('click', deleteAllTasks);
    
